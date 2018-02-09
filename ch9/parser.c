3524d2f1a54d1f1de7f852fe2563aff06fdc58d8
Adrian Dapprich <s8addapp@stud.uni-saarland.de>
2018-02-09 20:05:16 +0100

    Replaced builtin_op, eval and take

--- ch9/parser.c
+++ ch9/parser.c
@@ -125,106 +125,88 @@ lval* lval_read_num (mpc_ast_t* t) {
   return errno != ERANGE ? lval_num(x) : lval_err("invalid number"); 
 }
 
-/* Change to return null and set errno on error */
-lval* lval_pop (lval* v, int i) {
-  assert (i < v->count);
-
+lval* lval_pop(lval* v, int i) {
+  /* Find the item at "i" */
   lval* x = v->cell[i];
-  memmove (&v->cell[i], &v->cell[i+1], sizeof (lval*) * (v->count - i - 1));
+  /* Shift memory after the item at "i" over the top */memmove(&v->cell[i], &v->cell[i+1],
+							       sizeof(lval*) * (v->count-i-1));
+  /* Decrease the count of items in the list */
   v->count--;
-  v->cell = realloc (v->cell, sizeof (lval*) * v->count);
+  /* Reallocate the memory used */
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  return x;
+}
+lval* lval_take(lval* v, int i) {
+  lval* x = lval_pop(v, i);
+  lval_del(v);
   return x;
 }
 
-lval* lval_take (lval* v, int i) {
-  lval* x = lval_pop (v, i);
-  lval_del (v);
-  return x;
-}
-
-lval* builtin_op (lval* rands, char* rator) {
-  /* Check if all numbers */
-  for (int i = 0; i < rands->count; i++) {
-    if (rands->cell[i]->type != LVAL_NUM) {
-      lval_del (rands);
-      return lval_err ("Cannot operate on non-numbers.");
+lval* builtin_op(lval* a, char* op) {
+  /* Ensure all arguments are numbers */
+  for (int i = 0; i < a->count; i++) {
+    if (a->cell[i]->type != LVAL_NUM) {
+      lval_del(a);
+      return lval_err("Cannot operate on non-number!");
     }
   }
-
-  lval* x = lval_pop (rands, 0);
-
-  /* Perform unary negation if necessary */
-  /* todo does not work */
-  if ((strcmp (rator, "-"))
-      && (rands->count == 0)) {
-    x->num = - x->num;
+  /* Pop the first element */
+  lval* x = lval_pop(a, 0);
+  /* If no arguments and sub then perform unary negation */
+  if ((strcmp(op, "-") == 0) && a->count == 0) {
+    x->num = -x->num;
   }
-
-  while (rands->count > 0) {
-    lval* y = lval_pop (rands, 0);
-
-    
-    if (strcmp (rator, "+") == 0) { x->num += y->num; }
-    else if (strcmp (rator, "-") == 0) { x->num -= y->num; }
-    else if (strcmp (rator, "*") == 0) { x->num *= y->num; }
-    else if (strcmp (rator, "/") == 0) {
+  /* While there are still elements remaining */
+  while (a->count > 0) {/* Pop the next element */
+    lval* y = lval_pop(a, 0);
+    /* Perform operation */
+    if (strcmp(op, "+") == 0) { x->num += y->num; }
+    if (strcmp(op, "-") == 0) { x->num -= y->num; }
+    if (strcmp(op, "*") == 0) { x->num *= y->num; }
+    if (strcmp(op, "/") == 0) {
       if (y->num == 0) {
-	lval_del (x);
-	lval_del (rands);
-	lval_del (y);
-	return lval_err ("Division by zero!");
-      } else {
-	x->num /= y->num;
+	lval_del(x); lval_del(y);
+	x = lval_err("Division By Zero.");
+	break;
       }
-      break;
+      x->num /= y->num;
     }
-
-    lval_del (y);
+    /* Delete element now finished with */
+    lval_del(y);
   }
-
-  lval_del (rands);
+  /* Delete input expression and return result */
+  lval_del(a);
   return x;
 }
-
-lval* lval_eval (lval* v);
-
-lval* lval_eval_sexpr (lval* v) {
-
-  /* First evaluate the children */
+lval* lval_eval(lval* v);
+lval* lval_eval_sexpr(lval* v) {
+  /* Evaluate Children */
   for (int i = 0; i < v->count; i++) {
-    v->cell[i] = lval_eval (v->cell[i]);
+    v->cell[i] = lval_eval(v->cell[i]);
   }
-
-  /* Return the first error we find */
+  /* Error Checking */
   for (int i = 0; i < v->count; i++) {
-    if (v->cell[i]->type == LVAL_ERR) { return lval_take (v, i); }
+    if (v->cell[i]->type == LVAL_ERR) { return lval_take(v, i); }
   }
-
-  /* Empty expressions are just returned  */
+  /* Empty Expression */
   if (v->count == 0) { return v; }
-
-  /* Why would you just return single expressions. they should be evaluated */
-  if (v->count == 1) { return lval_take (v, 0); }
-
-  /* Ensure first element is symbol */
-  lval* f = lval_pop (v, 0);
+  /* Single Expression */
+  if (v->count == 1) { return lval_take(v, 0); }
+  /* Ensure First Element is Symbol */
+  lval* f = lval_pop(v, 0);
   if (f->type != LVAL_SYM) {
-    lval_del (f);
-    lval_del (v);
-    return lval_err ("S-expression does not start with symbol");
+    lval_del(f); lval_del(v);
+    return lval_err("S-expression Does not start with symbol.");
   }
-
-  /* Call builtin_op with operator */
-  lval* result = builtin_op (v, f->sym);
-  lval_del (f);
+  /* Call builtin with operator */
+  lval* result = builtin_op(v, f->sym);
+  lval_del(f);
   return result;
 }
-
-/* Eval lval */
-lval* lval_eval (lval* v) {
-  if (v->type == LVAL_SEXPR) { return lval_eval_sexpr (v); }
-
-  /* All other lvals stay the same */
+lval* lval_eval(lval* v) {
+  /* Evaluate Sexpressions */
+  if (v->type == LVAL_SEXPR) { return lval_eval_sexpr(v); }
+  /* All other lval types remain the same */
   return v;
 }
 
@@ -243,7 +225,6 @@ lval* lval_read (mpc_ast_t* t) {
   if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
 
   lval* x = NULL;
-
   /* todo allow for several expressions at top level. We prob need a (begin ...) construct for that  */
   if (strcmp(t->tag, ">") == 0) { return lval_read (t->children[1]); }
   /* if it's a sexpr create an empty list of sub lvals*/
@@ -301,11 +282,11 @@ int main (int argc, char**argv) {
       lval* res_read = lval_read (r.output);
       lval_println (res_read);
       
-      /* lval* res_eval = lval_eval (res_read); */
-      /* lval_println (res_eval); */
+      lval* res_eval = lval_eval (res_read);
+      lval_println (res_eval);
 
       lval_del (res_read);
-      /* lval_del (res_eval); */
+      lval_del (res_eval);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
